a8b6375689b4923e5cc13cb2e0746d06a2468542
Kagaya <kagaya85@outlook.com>
2020-11-13 01:33:29 +0800

    fix some bugs and made more

--- hellofudan/hellofudan.go
+++ hellofudan/hellofudan.go
@@ -21,6 +21,7 @@ const (
 	loginRedirectURL = "http://uis.fudan.edu.cn/authserver/index.do"
 	logoutURL        = "https://uis.fudan.edu.cn/authserver/logout?service=/authserver/login"
 	dailyFudanURL    = "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily"
+	saveURL          = "https://zlapp.fudan.edu.cn/ncov/wap/fudan/save"
 	checkURL         = "https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info"
 	userAgent        = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36 Edg/86.0.622.63"
 )
@@ -168,7 +169,7 @@ func (hf *HelloFudan) checkStatus() bool {
 		hf.log.Println("Today already checked in")
 		return true
 	}
-
+	hf.log.Println("Today haven't checked in")
 	return false
 }
 
@@ -176,27 +177,42 @@ func (hf *HelloFudan) checkIn() {
 	hf.log.Println("Start check in")
 
 	geoInfo := make(map[string]interface{})
-	json.Unmarshal(hf.info["geo_api_info"].([]byte), &geoInfo)
+	json.Unmarshal([]byte(hf.info["geo_api_info"].(string)), &geoInfo)
 	addr := geoInfo["addressComponent"].(map[string]interface{})
 
+	province, has := addr["province"].(string)
+	if !has {
+		province = ""
+	}
+	city, has := addr["city"].(string)
+	if !has {
+		city = ""
+	}
+	district, has := addr["district"].(string)
+	if !has {
+		district = ""
+	}
+
 	hf.info["tw"] = "13"
-	hf.info["province"] = addr["province"]
-	hf.info["city"] = addr["city"]
-	hf.info["area"] = strings.Join([]string{addr["province"].(string), addr["city"].(string), addr["district"].(string)}, " ")
+	hf.info["province"] = province
+	hf.info["city"] = city
+	hf.info["area"] = strings.Join([]string{province, city, district}, " ")
 
 	hf.log.Printf("Check in position: %s", hf.info["area"])
 
-	data, err := json.Marshal(hf.info)
-	if err != nil {
-		panic(err)
+	// need to convert map[string]interface{} to url.Values
+	data := url.Values{}
+	for k, v := range hf.info {
+		data.Add(k, v)
 	}
 
-	body := strings.NewReader(string(data))
-	req, _ := http.NewRequest("POST", loginURL, body)
+	body := strings.NewReader(data.Encode())
+	req, _ := http.NewRequest("POST", saveURL, body)
 
 	req.Header.Set("Host", "zlapp.fudan.edu.cn")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Referer", "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily?from=history")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Origin", "https://zlapp.fudan.edu.cn")
+	req.Header.Set("Referer", dailyFudanURL+"?from=history")
 	req.Header.Set("User-Agent", userAgent)
 	req.Header.Set("DNT", "1")
 	req.Header.Set("TE", "Trailers")
@@ -207,10 +223,13 @@ func (hf *HelloFudan) checkIn() {
 	}
 	defer resp.Body.Close()
 
-	data, _ = ioutil.ReadAll(resp.Body)
-	var v interface{}
-	json.Unmarshal(data, &v)
-	msg := v.(map[string]interface{})["m"]
-
-	hf.log.Printf("Check in message: %s", msg)
+	if resp.StatusCode == 200 {
+		data, _ := ioutil.ReadAll(resp.Body)
+		ret := make(map[string]interface{})
+		json.Unmarshal(data, &ret)
+		msg := ret["m"].(string)
+		hf.log.Printf("Check in message: %s", msg)
+	} else {
+		hf.log.Printf("Check in failed: %s", resp.Status)
+	}
 }
